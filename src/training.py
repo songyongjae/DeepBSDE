e554fec70fa12944ef6cd4c232b5f6ae682463b1
SYJ <131941451+songyongjae@users.noreply.github.com>
2024-12-19 21:58:00 +0900

    Delete src directory

--- src/training.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-def train_model(model, num_iterations=1000):
-    for i in range(num_iterations):
-        inp = model.draw_X_and_dW(30)
-        model.train(inp)
-        print(f"Iteration {i}: u0={model.u0}, Loss={model.loss_fn(inp)}")
