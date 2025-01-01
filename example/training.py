283dae20df3de0a1e8e486110c050d59e3689e7c
SYJ <131941451+songyongjae@users.noreply.github.com>
2025-01-02 03:05:02 +0900

    Delete example directory

--- example/training.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-def train_model(model, num_iterations=1000):
-    for i in range(num_iterations):
-        inp = model.draw_X_and_dW(30)
-        loss = model.train(inp)
-        print(f"Iteration {i}: u0={model.u0.numpy()}, Loss={loss.numpy()}")
